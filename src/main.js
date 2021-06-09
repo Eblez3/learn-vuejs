feb3734097ec04980dd24cb09e7dad39f21afde6
Pierre-Louis COSSON <cosson.pl@hotmail.fr>
2021-06-09 14:53:23 +0200

    Première instance de vue et appel

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css' 
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  data: {
+    user: 'Bertrant'
+  },
 })
